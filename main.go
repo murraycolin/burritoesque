f11e3e4f1ec635783985083a8896d318383617b1
Colin Murray <github@murraycolin.com>
2018-04-29 19:03:24 -0700

    Small changes to timeout

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"math"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	request, err := http.NewRequest("GET", "https://www.cmurrayclient.com/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0")
+	t := time.Now().Hour()
+	tf := float64(t)
+	index := math.Sin(tf/math.Pi) + 1
+	for index < 2 {
+		response, err := client.Do(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		index = index + .005
+		defer response.Body.Close()
+	}
+}
